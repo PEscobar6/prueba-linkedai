0abbcd259435b1cde9ee7e4d19b736d79b90ec8b
Peescobar <pablo-9906@outlook.es>
2022-02-10 22:36:39 -0500

    firts commit

---
+++ models/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server{
+    constructor(){
+        this.app    = express();
+        this.port   = process.env.PORT;
+
+        // Path
+        this.usersPath = '/api/users'
+
+        //Middlewares
+        this.middlewares();
+
+        // Routes
+        this.routes();
+    }
+
+    middlewares(){
+
+        // CORS
+        this.app.use(cors());
+
+        // Read and parse body
+        this.app.use(express.json());
+
+        // Public Directory
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.use(this.usersPath, require('../routes/user'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+}
+
+module.exports = Server;
